0252f2a0e5ba76df5c3abfd887aa692b00d14eda
HenryFerreira <henryferreira133@gmail.com>
2022-02-22 21:51:40 -0300

    Se implemento el editar usuario y se finalizo con el video

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import UserTable from './components/UserTable';
 import { v4 as uuidv4 } from 'uuid';
 import AddUserForm from './components/AddUserForm';
+import EditUserForm from './components/EditUserForm';
 
 function App() {
 
@@ -28,17 +29,61 @@ function App() {
     setUsers(users.filter(user => user.id !== id))
   }
 
+  //Editar usuario
+  const [editing, setEditing] = useState(false);
+  const [currentUser, setCurrentUser] = useState({
+    id: null,
+    name: '',
+    username: ''
+  });
+
+  const editRow = (user) => {
+    setEditing(true);
+    setCurrentUser({
+      id: user.id,
+      name: user.name,
+      username: user.username
+    })
+  }
+
+  const updateUser = (id, updateUser) => {
+    setEditing(false);
+    setUsers(users.map(user => (user.id === id ? updateUser : user)))
+  }
+
+
   return (
     <div className='container'>
       <h1>CRUD app with hooks</h1>
       <div className='flex-row'>
         <div className='flex-large'>
-          <h2>Add user</h2>
-          <AddUserForm addUser={addUser}/>
+          {
+            editing?(
+              <div>
+                <h2>Editing</h2>
+                <EditUserForm
+                  currentUser={currentUser}
+                  updateUser={updateUser}
+                />
+              </div>
+            ) : (
+              <div>
+                <h2>Add user</h2>
+                <AddUserForm addUser={addUser}/>    
+              </div>
+            )
+          }
+
+
+          
         </div>
         <div className='flex-large'>
           <h2>view users</h2>
-          <UserTable users={users} deleteUser={deleteUser}/>
+          <UserTable 
+            users={users} 
+            deleteUser={deleteUser} 
+            editRow={editRow}
+          />
         </div>
       </div>
     </div>
---
+++ src/components/EditUserForm.jsx
@@ -0,0 +1,65 @@
+import React, { Fragment } from 'react'
+import { useForm } from 'react-hook-form';
+
+const EditUserForm = (props) => {
+
+    console.log(props.currentUser)
+
+    const {register, handleSubmit, formState: {errors}, setValue} = useForm(
+        {defaultValues: props.currentUser}
+    )
+
+    setValue('name', props.currentUser.name);
+    setValue('username', props.currentUser.username);
+
+    const onSubmit = (data, e) => {
+        console.log(data);
+        data.id = props.currentUser.id;
+        props.updateUser(props.currentUser.id, data);
+        e.target.reset();
+    }
+
+    return (
+        <Fragment>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>Name</label>
+                <input 
+                    type="text" 
+                    //Nueva forma de hacer los registers desde la v7
+                    {...register("name", {
+                        required: {
+                            value: true,
+                            message: 'Campo obligatorio'
+                        }
+                    }) 
+                    }
+                />
+                <div>
+                    {errors?.name?.message}
+                </div>
+
+                <label>Username</label>
+                <input 
+                    type="text" 
+                    //Nueva forma de hacer los registers desde la v7
+                    {...register("username", {
+                        required: {
+                            value: true,
+                            message: 'Campo obligatorio'
+                        }
+                    }) 
+                    }
+                />
+                <div>
+                    {errors?.username?.message}
+                </div>
+
+
+
+                <button>Edit user</button>
+            </form> 
+        </Fragment>
+    );
+}
+ 
+export default EditUserForm;
